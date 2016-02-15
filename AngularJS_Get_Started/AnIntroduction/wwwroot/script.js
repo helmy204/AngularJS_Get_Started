bba4cbf301d301bcd4ac510feeccb5b95abaf3aa
helmy204 <hamadaelking204@hotmail.com>
2016-02-15 11:44:44 +0200

    07 Modules | revealing module pattern

--- AngularJS_Get_Started/AnIntroduction/wwwroot/script.js
+++ AngularJS_Get_Started/AnIntroduction/wwwroot/script.js
@@ -1,22 +1,60 @@
 ﻿
-var work = function () {
-    console.log("working hard!");
+///////////////////////////////////////////////////
+
+// function abstraction pattern
+
+//var work = function () {
+//    console.log("working hard!");
+//};
+
+////work();
+
+//var doWork = function (f) {
+
+//    console.log("starting");
+
+//    try{
+//        f();
+//    }
+//    catch (ex) {
+//        console.log(ex);
+//    }
+
+//    console.log("end");
+//};
+
+//doWork(work);
+
+///////////////////////////////////////////////////
+
+// revealing module pattern
+
+var createWorker = function () {
+
+    var workCount = 0;
+
+    var task1 = function () {
+        workCount += 1;
+        console.log("task1 "+workCount);
+    };
+
+    var task2 = function () {
+        workCount += 1;
+        console.log("task2 "+workCount);
+    };
+
+    return {
+        job1: task1,
+        job2: task2
+    };
+
 };
 
-//work();
+var worker = createWorker();
 
-var doWork = function (f) {
-
-    console.log("starting");
-
-    try{
-        f();
-    }
-    catch (ex) {
-        console.log(ex);
-    }
-
-    console.log("end");
-};
-
-doWork(work);
+worker.job1();
+worker.job2();
+worker.job2();
+worker.job2();
+worker.job2();
+worker.job2();
