2dec67af26c1195acbaa0b3d83d0201068c37e55
helmy204 <hamadaelking204@hotmail.com>
2016-02-15 11:51:31 +0200

    08 The IIFE | Immediately invoked function expression (IIFE)

--- AngularJS_Get_Started/AnIntroduction/wwwroot/script.js
+++ AngularJS_Get_Started/AnIntroduction/wwwroot/script.js
@@ -29,32 +29,35 @@
 
 // revealing module pattern
 
-var createWorker = function () {
+// Immediately invoked function expression (IIFE)
+// pronounced as "iffy"
+(function () {
 
-    var workCount = 0;
+    var createWorker = function () {
 
-    var task1 = function () {
-        workCount += 1;
-        console.log("task1 "+workCount);
+        var workCount = 0;
+
+        var task1 = function () {
+            workCount += 1;
+            console.log("task1 " + workCount);
+        };
+
+        var task2 = function () {
+            workCount += 1;
+            console.log("task2 " + workCount);
+        };
+
+        return {
+            job1: task1,
+            job2: task2
+        };
+
     };
 
-    var task2 = function () {
-        workCount += 1;
-        console.log("task2 "+workCount);
-    };
+    var worker = createWorker();
 
-    return {
-        job1: task1,
-        job2: task2
-    };
-
-};
-
-var worker = createWorker();
-
-worker.job1();
-worker.job2();
-worker.job2();
-worker.job2();
-worker.job2();
-worker.job2();
+    worker.job1();
+    worker.job2();
+    worker.job2();
+    worker.job2();
+}());
