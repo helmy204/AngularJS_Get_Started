9f884995ca18cb206316b5eacf8fa35a4cfb2f08
helmy204 <hamadaelking204@hotmail.com>
2016-02-15 14:37:37 +0200

    02 Controller Basics

---
+++ AngularJS_Get_Started/Controllers/wwwroot/script.js
@@ -0,0 +1,12 @@
+﻿(function () {
+
+    angular
+        .module('app')
+        .controller('MainController', MainController);
+
+    function MainController($scope) {
+        $scope.message = "Hello, Angular!";
+    };
+
+}());
+
