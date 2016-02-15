0ce93fccf97c250cdf4b90f01327f715b5c87569
helmy204 <hamadaelking204@hotmail.com>
2016-02-15 14:50:22 +0200

    03 The Capabilities

--- AngularJS_Get_Started/Controllers/wwwroot/script.js
+++ AngularJS_Get_Started/Controllers/wwwroot/script.js
@@ -5,7 +5,15 @@
         .controller('MainController', MainController);
 
     function MainController($scope) {
+
+        var person = {
+            firstName: "Scott",
+            lastName: "Allen",
+            imageSrc:"http://odetocode.com/Images/scott_allen_2.jpg"
+        };
+
         $scope.message = "Hello, Angular!";
+        $scope.person = person;
     };
 
 }());
