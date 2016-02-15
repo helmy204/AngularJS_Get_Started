249cb2bcf78027cfffae3aa9e6fc219dd5eaf060
helmy204 <hamadaelking204@hotmail.com>
2016-02-15 10:57:36 +0200

    06 JavaScript Patterns | Functions as abstractions.

---
+++ AngularJS_Get_Started/AnIntroduction/wwwroot/script.js
@@ -0,0 +1,22 @@
+﻿
+var work = function () {
+    console.log("working hard!");
+};
+
+//work();
+
+var doWork = function (f) {
+
+    console.log("starting");
+
+    try{
+        f();
+    }
+    catch (ex) {
+        console.log(ex);
+    }
+
+    console.log("end");
+};
+
+doWork(work);
