0e79ca13c7d483acdfcd26aa3db5ae9406b056c6
helmy204 <hamadaelking204@hotmail.com>
2016-02-16 08:59:43 +0200

    03 ng-model

---
+++ AngularJS_Get_Started/DirectivesAndViews/wwwroot/_references.js
@@ -0,0 +1,4 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="app/app.module.js" />
+/// <reference path="lib/angular/angular.js" />
+/// <reference path="script.js" />
---
+++ AngularJS_Get_Started/DirectivesAndViews/wwwroot/script.js
@@ -0,0 +1,32 @@
+﻿(function () {
+
+    //angular.module('githubViewer').controller('MainController', MainController);
+    angular.module('githubViewer').controller('MainController', ["$scope", "$http", MainController]);
+
+
+    function MainController($scope, $http) {
+
+        var onUserComplete = function (response) {
+            $scope.user = response.data;
+        };
+
+        var onError = function (reason) {
+            $scope.error = "Could not fetch the user";
+        }
+
+
+        // https://api.github.com/users/odetocode
+        // https://api.github.com/users/helmy204
+        // https://api.github.com/users/robconery
+        // https://api.github.com/users/angular
+
+        $http.get("https://api.github.com/users/angular")
+             .then(onUserComplete,onError);
+
+        $scope.username = "angular";
+        $scope.message = "GitHub Viewer";
+
+    };
+
+}());
+
