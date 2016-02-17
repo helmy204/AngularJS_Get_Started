97e5ef4b11f28b292d60b8415f735e8018607734
helmy204 <hamadaelking204@hotmail.com>
2016-02-17 08:48:30 +0200

    05 ng-repeat

--- AngularJS_Get_Started/DirectivesAndViews/wwwroot/script.js
+++ AngularJS_Get_Started/DirectivesAndViews/wwwroot/script.js
@@ -8,7 +8,13 @@
 
         var onUserComplete = function (response) {
             $scope.user = response.data;
+            $http.get($scope.user.repos_url)
+                 .then(onRepos, onError);
         };
+
+        var onRepos = function (response) {
+            $scope.repos = response.data;
+        }
 
         var onError = function (reason) {
             $scope.error = "Could not fetch the user";
